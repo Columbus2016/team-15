18376aa5a1dd198cbb852dfcadd3c996e6cdee63
brownaj3 <brownaj3@miamioh.edu>
2016-10-15 09:28:28 -0400

    email alter added

---
+++ js/email.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function(){
+    var $emailBtn = $('#emailBtn');
+    function email() {
+        var connstring = "mailto:escape(document.getElementById('').value)?body=Come join Feeding America at this link www.fake.com  it is really fun";
+        window.location.href = connstring;
+    }
+
+    $emailBtn.on('click', function () {
+        email();
+        alert("Email has been sent");
+    });
+});
